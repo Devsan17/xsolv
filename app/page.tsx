afc542a7793dcc0f7440f78b6b046d51711bbd10
sanX17 <sanxofficial17@gmail.com>
2026-08-16 00:14:16 +0530

    V1.2

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
               </div>
 
               {/* Floating Black Integration Bar - positioned at bottom */}
-              <div className="absolute -bottom-8 left-1/2 -translate-x-1/2 z-20 w-[70%] px-8">
+              <div className="absolute bottom-0 left-1/2 -translate-x-1/2 z-20 w-[70%] px-8">
                 <div className="rounded-t-full bg-black shadow-[0_20px_40px_rgba(0,0,0,0.15)] px-16 py-8 flex items-center justify-center">
                   <p className="text-3xl font-bold text-white tracking-[0.2em]">
                     Solve support succeed
